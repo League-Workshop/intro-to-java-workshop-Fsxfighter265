b54952c2d544efa324993000bc8294268832a4c6
Fsxfighter265 <league@WorthyMini-6.attlocal.net>
2017-11-20 13:52:12 -0800

    Day 2 Workshop

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -8,8 +8,28 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
+
+		String str = "end world!";
+	    str = str.toUpperCase();
+		System.out.println(str);
+		String Boi = "gjl;fdkgjfdlk;gjfdlgjfd;lgjflk;gjfxdl;kbjdflk;gjdflk;gfds;lkgjdfsl;k";
+		int Boii = Boi.length();
+		System.out.println(Boii);
+
+		
+		System.out.println("Hello World");
+		
+		Robot myRobot = new Robot();
+		myRobot.setRandomPenColor();
+		myRobot.penDown();
+		myRobot.setSpeed(1000);
+		
+		for (int i=1; i<40; i++) {
+		myRobot.move(200);
+	    myRobot.turn(90);
+	    
+		}
 		
 		
-	}
+	}	
 }
